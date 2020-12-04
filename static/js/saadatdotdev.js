cea69c558aa4ffd291afcca3f42753c2b8b7a50c
mass1ve-err0r <saadatdev@googlemail.com>
2020-12-04 01:59:49 +0100

    add prototype js + css

---
+++ static/js/saadatdotdev.js
@@ -0,0 +1,9 @@
+new TypeIt("#introText", {
+    strings: ["Nice to meet you!", "I'm Mirza Saadat Baig"],
+    speed: 75,
+    cursor: false,
+    loop: false,
+    waitUntilVisible: false,
+    breakLines: false,
+    nextStringDelay: 1000,
+}).go();
