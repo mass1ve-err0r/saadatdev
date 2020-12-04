36afdb3a8694faf0ff7e6b7a4c142f7f29b6938a
mass1ve-err0r <saadatdev@googlemail.com>
2020-12-04 18:33:38 +0100

    clean out js (keep it at a minimum bro)

--- static/js/saadatdotdev.js
+++
@@ -1,9 +0,0 @@
-new TypeIt("#introText", {
-    strings: ["Nice to meet you!", "I'm Mirza Saadat Baig"],
-    speed: 75,
-    cursor: false,
-    loop: false,
-    waitUntilVisible: false,
-    breakLines: false,
-    nextStringDelay: 1000,
-}).go();
