f5a4350bf279d111cdf52bf77825c9cbcdc9dab8
mass1ve-err0r <saadatdev@googlemail.com>
2020-12-13 16:52:03 +0100

    add unified exception handler

--- server.py
+++ server.py
@@ -15,6 +15,16 @@ J2env = Environment(loader=PackageLoader('server', './templates'),
 J2env.globals["url_for"] = app.url_for
 
 
+# -*- Sanic Extensions -*-
+from sanic.exceptions import SanicException
+from sanic.response import html
+@app.exception(SanicException)
+async def catch_exceptions_4xx_5xx(req, excp):
+    template = J2env.get_template('/pages/Error_4xx_5xx.jinja2')
+    _html = await template.render_async(title="Whoops! | Saadat Baig Development")
+    return html(_html)
+
+
 # -*- Blueprint Registration -*-
 from Blueprints.Home import HomeBP
 app.blueprint(HomeBP)
